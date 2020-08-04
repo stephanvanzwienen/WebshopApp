5667e6c0d874d8c692584818d046d36131f43a8c
Stephan van Zwienen <stephanvanzwienen@gmail.com>
2020-08-04 11:11:58 +0200

    added form input fields for adding product

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products_provider.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products.dart';
+import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -41,6 +42,7 @@ class MyApp extends StatelessWidget {
             CartScreen.routeName: (ctx) => CartScreen(),
             OrdersScreen.routeName: (ctx) => OrdersScreen(),
             UserProductsScreen.routeName: (ctx)=> UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
           }),
     );
   }
